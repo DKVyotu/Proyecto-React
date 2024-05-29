1fbc7631d6f8e8fbcc4a1eaeffbe05b39ce7732d
Antonio Hurtado <DKVantonio@gmail.com>
2024-05-28 21:35:36 -0500

    img CTA

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom'
+import ImagenCTA from '../../assets/imagenVinos.jpg'
+
 
 const Home = () => {
   return (
-    <div className="footerAbajo">Hola Mundo</div>
+    <div className="footerAbajo">
+      <div className="d-flex justify-content-center">
+        <div className="d-flex flex-column align-self-center px-5">
+          <h2 className='pb-5'>Un CTA llamativo...</h2>
+          <Link to={"/Productos"} class="btn btn-warning align-self-center">Ver mas...</Link>
+        </div>
+        <img src={ImagenCTA} alt="" width={500}/>
+      </div>
+    </div>
   )
 }
 
